c885e1a7a027b081818a1beea9888e31391c93ca
Alexander <alez.grante@gmail.com>
2022-01-24 14:33:12 +0300

    WebView with JsonParsing

--- app/src/main/java/com/example/webrequests/MainActivity.kt
+++ app/src/main/java/com/example/webrequests/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.webrequests
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -7,6 +8,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.webrequests.databinding.ActivityMainBinding
 import kotlinx.coroutines.*
+import org.json.JSONObject
 import java.io.BufferedInputStream
 import java.io.InputStream
 import java.net.HttpURLConnection
@@ -26,13 +28,14 @@ class MainActivity : AppCompatActivity() {
     fun setOnclickListener(){
         binding.buttonRequest.setOnClickListener {
             CoroutineScope(Dispatchers.IO + Job()).launch{
-                val url = URL(URL_ADDRESS)
+                val url = URL(BASE_URL)
                 val urlConnection = url.openConnection() as HttpURLConnection
                 try{
                     val inputStream: InputStream = BufferedInputStream(urlConnection.inputStream)
-                    val text = inputStream.bufferedReader().readText()
+                    val jsonStr = inputStream.bufferedReader().readText()
+                    val mediaUrl = getMediaUrl(jsonStr)
                     Handler(Looper.getMainLooper()).post {
-                        toast(text)
+                        setWebView(mediaUrl)
                     }
                 }finally {
                     urlConnection.disconnect()
@@ -42,11 +45,18 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun toast(mes: String) {
-        Toast.makeText(this, mes, Toast.LENGTH_LONG).show()
+    fun getMediaUrl(jsonStr: String): String = JSONObject(jsonStr).getString(URL)
+
+    fun setWebView(url: String) {
+        with(binding.webView){
+            settings.javaScriptEnabled = true
+            loadUrl(url)
+        }
+
     }
 
     companion object {
-        private const val URL_ADDRESS = "https://random.dog/woof.json"
+        private const val URL = "url"
+        private const val BASE_URL = "https://random.dog/woof.json"
     }
 }
