d125149609b2ce26f3d533a0ff5b50160c2e452c
Alexander <alez.grante@gmail.com>
2022-01-24 13:47:21 +0300

    HttpUrlConnection

--- app/src/main/java/com/example/webrequests/MainActivity.kt
+++ app/src/main/java/com/example/webrequests/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.webrequests
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.webrequests.databinding.ActivityMainBinding
-import java.net.URLConnection
+import kotlinx.coroutines.*
+import java.io.BufferedInputStream
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,13 +17,34 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        binding.buttonRequest.setOnClickListener {
-
-        }
+        setOnclickListener()
         setContentView(binding.root)
     }
 
-    companion object{
-        private const val URL_ADDRESS = "http://google.com"
+    fun setOnclickListener(){
+        binding.buttonRequest.setOnClickListener {
+            CoroutineScope(Dispatchers.IO + Job()).launch{
+                val url = URL(URL_ADDRESS)
+                val urlConnection = url.openConnection() as HttpURLConnection
+                try{
+                    val inputStream: InputStream = BufferedInputStream(urlConnection.inputStream)
+                    val text = inputStream.bufferedReader().readText()
+                    runOnUiThread {
+                        toast(text)
+                    }
+                }finally {
+                    urlConnection.disconnect()
+                }
+            }
+
+        }
+    }
+
+    fun toast(mes: String) {
+        Toast.makeText(this, mes, Toast.LENGTH_LONG).show()
+    }
+
+    companion object {
+        private const val URL_ADDRESS = "https://random.dog/woof.json"
     }
 }
